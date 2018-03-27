2e516621d6024aa6e6cecd40b3a1ab4fbf425def
peakcodes <35933644+peakcodes@users.noreply.github.com>
2018-03-27 12:55:17 -0400

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,43 @@ const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
+	
+	app.post("/api/search", (req, res) => {
+
+		db.Med.findAll({}).then(results => {
+			res.json(results);
+		})
+	});
+	app.get('/search/:searchTerm?', (req, res)=>{
+		let searchTerm = req.params.searchTerm;
+		if (searchTerm) {
+			axios.get(`https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`).then(function (data) {
+				let obj = data.data.results;
+				// console.log(req);
+				let parsedData = [];
+				Object.keys(obj).forEach(function (key) {
+					let brandName = obj[key].openfda.brand_name[0];
+					let genericName = obj[key].openfda.generic_name[0];
+					let route = obj[key].openfda.route[0];
+					let whenUsing = obj[key].when_using[0];
+					let purpose = obj[key].purpose[0];
+					let doseAdmin = obj[key].dosage_and_administration[0];
+					let activeIngredient = obj[key].active_ingredient[0];
+					let question = obj[key].questions[0];
+					parsedData.push([brandName, genericName, route, whenUsing, purpose, doseAdmin, activeIngredient, question]);
+				});
+				console.log(parsedData);
+				res.render("../views/search.handlebars", {parsedData: parsedData});
+			});
+		}
+		else {
+			res.render("search")
+		}
+	});
+	app.post("/fdasearch", (req, res) => {
+		let searchTerm = req.body.searchTerm;
+		res.redirect(`/search/${searchTerm}`);
+	})
 
 	app.get("/api/meds", (req, res) => {
 
